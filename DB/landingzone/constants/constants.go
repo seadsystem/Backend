90f14de14ce5b7569b582ff7e197b790c8b5539f
Ian <git@loosescre.ws>
2014-11-05 19:12:44 -0800

    Regex attempt 3

--- DB/landingzone/constants/constants.go
+++ DB/landingzone/constants/constants.go
@@ -9,5 +9,5 @@ const (
 	ACK                = "\x06\n"
 	OKAY               = "@K\n"
 	READ_TIME_LIMIT    = 5
-	HEADER_REGEX       = "/(?:THS)([0-9]+)/"
+	HEADER_REGEX       = "/(?:THS)([0-9]+)/g"
 )
