81eb0f172e943400295e9629400c0a4b7c093b2f
Ian <git@loosescre.ws>
2014-11-05 17:39:55 -0800

    Added header decoding.

--- DB/landingzone/decoders/decoders.go
+++ DB/landingzone/decoders/decoders.go
@@ -1,20 +1,46 @@
 package decoders
-package math
 
-import ()
+import (
+	"math"
+	"regexp"
+	"errors"
+	"strconv"
+	
+	"github.com/seadsystem/Backend/DB/landingzone/constants"
+)
+
+var headerRegex *Regexp
+var InvalidHeader = errors.New("Invalid header.")
+
+func init() {
+	headerRegex, err := regexp.Compile(constants.HEADER_REGEX)
+	if err != nil {
+		log.Println("Regex compile error:", err)
+	}
+}
 
 // Extract data sent from sensor from request
-func decodePacket(buffer []byte) {
+func DecodePacket(buffer []byte) {
 
 }
 
-func double_to_ascii_time(double_time float64) (string) {
-   var days = Floor(double_time / (60*60*24))
-   var hours = Floor((double_time % (60*60*24)) / (60*60))
-   var minutes = Floor((double_time % (60*60))   / (60))
-   var seconds = Floor((double_time % (60)) / (1))
-   var milliseconds = Floor(((double_time * 1000) %1000))
-   var clock_time = Floor(((double_time * 12000) %12))
+func DecodeHeader(packet []byte) (serial int, err error) {
+	serialString = headerRegex.Find(packet)
+	if serialString == nil {
+		err = InvalidHeader
+		return
+	}
+	serial, err = strconv.Atoi(string(serialString))
+	return
+}
+
+func doubleToAsciiTime(double_time float64) (string) {
+   var days = math.Floor(double_time / (60*60*24))
+   var hours = math.Floor((double_time % (60*60*24)) / (60*60))
+   var minutes = math.Floor((double_time % (60*60))   / (60))
+   var seconds = math.Floor((double_time % (60)) / (1))
+   var milliseconds = math.Floor(((double_time * 1000) %1000))
+   var clock_time = math.Floor(((double_time * 12000) %12))
 
    return fmt.Sprintf("%03d%02d%02d%02d%03d%02d", days, hours, minutes, seconds, milliseconds, clock_time)
 }
