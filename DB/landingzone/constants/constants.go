0e9c7e7c7e2aaa855666ae6607fca55b21d2444a
Ian <git@loosescre.ws>
2014-11-08 03:47:19 -0800

    Trying fuller config.

--- DB/landingzone/constants/constants.go
+++ DB/landingzone/constants/constants.go
@@ -10,11 +10,12 @@ const (
 	HEAD   = "@H\n"
 	ACK    = "\x06\n"
 	OKAY   = "@K\n"
-	CONFIG = "@SLITIsEP00000000500000C00001\n" // Config value is no longer used by plug. We must still send it a valid config anyway.
+	//CONFIG = "@SLITIsEP00000000500000C00001\n" // Config value is no longer used by plug. We must still send it a valid config anyway.
 	// Full config includes these lines too:
 	//"@SLITVsEP00000000500000C00001\n"
 	//"@SLITWsEP00000000500000C00001\n"
 	//"@SLITTsEP00000000500000C00001\n"
+	CONFIG = "@SLITIsEP00000000500000C00001\n@SLITVsEP00000000500000C00001\n@SLITWsEP00000000500000C00001\n@SLITTsEP00000000500000C00001\n"
 
 	READ_TIME_LIMIT = 5 * 60 // Measured in seconds
 	DB_TIME_LIMIT   = 10     // Measured in seconds
