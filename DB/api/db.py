f0658f72a3f0d3296939c39f709a543ff9438110
Raymond Colebaugh <raymondcolebaugh@gmail.com>
2014-11-10 08:50:51 -0800

    Initial work on query code for API

--- DB/api/db.py
+++ DB/api/db.py
@@ -1,4 +1,10 @@
 # Python 3
+import psycopg2
+import psycopg2.extras
+
+# Database user credentials
+DATABASE = ""
+USER	 = ""
 
 def query(parsed_url):
 	'''
@@ -59,50 +65,52 @@ def retrieve_within_timeframe(device_id, start_time, end_time):
 	'''
 	   Return sensor data for a device within a specified timeframe
 	'''
-	data = [
-		['Time', 'KW/H', 'Temp'],
-		['1', 50, 70],
-		['2', 60, 77],
-		['3', 80, 82],
-		['4', 50, 76],
-		['5', 50, 70],
-		['6', 50, 70],
-		['7', 60, 77],
-		['8', 80, 82],
-		['9', 50, 76],
-		['10', 50, 70]
-	]
-	return data
+	query = "SELECT * FROM crosstab(" +\
+				"'SELECT time, type, data from data_raw " +\
+				"WHERE serial = %s AND time BETWEEN %s AND %s'" +\
+			") AS ct_result(time TIMESTAMP, I DECIMAL, W DECIMAL, V DECIMAL, T DECIMAL);"
+	params = (int(device_id), start_time, end_time)
+	rows = perform_query(query, params)
+	return rows
 
 def retrieve_historical(device_id):
 	'''
 	   Return sensor data for a specific device
+	   TODO: add a page size limit?
 	'''
-	data = [
-		['Time', 'KW/H', 'Temp'],
-		['1', 50, 70],
-		['2', 60, 77],
-		['3', 80, 82],
-		['4', 50, 76],
-		['5', 50, 70],
-		['6', 50, 70],
-		['7', 60, 77],
-		['8', 80, 82],
-		['9', 50, 76],
-		['10', 50, 70],
-		['11', 50, 70],
-		['12', 60, 77],
-		['13', 80, 82],
-		['14', 50, 76],
-		['15', 50, 70],
-		['16', 50, 70],
-		['17', 60, 77],
-		['18', 80, 82],
-		['19', 50, 76],
-		['20', 50, 70]
-	]
-	return data
+	query = "SELECT * FROM crosstab(" +\
+				"'SELECT time, type, data from data_raw " +\
+				"WHERE serial = %s'" +\
+			") AS ct_result(time TIMESTAMP, I DECIMAL, W DECIMAL, V DECIMAL, T DECIMAL);"
+	params = (int(device_id), )
+	rows = perform_query(query, params)
+	return rows
+
+def perform_query(query, params):
+	'''
+		Initiate a connection to the database and return a cursor
+		to return db rows a dictionaries
+		Returns cursor
+	'''
+	con = None
+	try:
+		con = psycopg2.connect("dbname='" + DATABASE +
+				"' user='" + USER + "'")
+		cursor = con.cursor()
+		cursor.execute(query, params)
+		return cursor.fetchall()
+
+	except psycopg2.DatabaseError as e:
+		print('Database error: %s' % e)
+
+	finally:
+		if con:
+			con.close()
 
 def format_data(data, format):
+	'''
+		Process rows of data returned by the db and format
+		them appropriately
+	'''
 	if format == 'array':
 		return str(data)
