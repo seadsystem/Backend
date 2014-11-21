d91c7c75ad5582f5dffc7ff83ec9a3c89dd47188
Ian <git@loosescre.ws>
2014-11-20 19:38:51 -0800

    Added usage message.

--- DB/api/handler.py
+++ DB/api/handler.py
@@ -3,6 +3,8 @@ import http.server
 import url_parser
 import db
 
+USAGE = "/(device id)?[start_time=(start time as UTC unix timestamp), end_time=(end time as UTC unix timestamp), type=(Sensor type code)].join('&')"
+
 
 class ApiHandler(http.server.SimpleHTTPRequestHandler):
 
@@ -16,18 +18,25 @@ class ApiHandler(http.server.SimpleHTTPRequestHandler):
 			self.send_error(404)
 			return
 
+		if parsed["device_id"] is None:
+			self.send_response(200)
+			self.send_header("Content-type", "text/plain")
+			self.end_headers()
+			self.wfile.write((self.server() + USAGE).encode("utf-8"))
+
 		try:
 			r = db.query(parsed)
+
+			self.send_response(200)
+			self.send_header("Content-type", "application/json;charset=utf-8")
+
+			self.end_headers()
+			self.wfile.write('[\n'.encode("utf-8"))
+			for line in r:
+				self.wfile.write(line.encode("utf-8"))
+			self.wfile.write(']\n'.encode("utf-8"))
 		except:
 			self.send_error(500)
 			return
 
-		self.send_response(200)
-		self.send_header("Content-type", "application/json;charset=utf-8")
-
-		self.end_headers()
-		self.wfile.write('[\n'.encode("utf-8"))
-		for line in r:
-			self.wfile.write(line.encode("utf-8"))
-		self.wfile.write(']\n'.encode("utf-8"))
 		self.wfile.flush()
