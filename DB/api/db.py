887a89579e692f4910c2aa13f89553ee94cab683
Raymond Colebaugh <raymondcolebaugh@gmail.com>
2014-11-20 20:45:17 -0800

    Updated API to allow more flexible GET params

--- DB/api/db.py
+++ DB/api/db.py
@@ -4,7 +4,7 @@ import functools
 
 # Database user credentials
 DATABASE = "seads"
-USER     = "seadapi"
+USER	 = "seadapi"
 
 
 def query(parsed_url):
@@ -14,33 +14,35 @@ def query(parsed_url):
 	:param parsed_url: Array of url parameters
 	:return: Generator of result strings
 	"""
-	if 'type' in parsed_url.keys():
-		results = retrieve_by_type(
-			parsed_url['device_id'],
-			parsed_url['start_time'],
-			parsed_url['end_time'],
-			parsed_url['type'],
-		)
-		header = ['time', parsed_url['type']]
-	elif 'start_time' in parsed_url.keys():
-		results = retrieve_within_timeframe(
-			parsed_url['device_id'],
-			parsed_url['start_time'],
-			parsed_url['end_time']
-		)
-		header = ['time', 'I', 'W', 'V', 'T']
-	elif 'device_id' in parsed_url.keys():
-		results = retrieve_historical(parsed_url['device_id'])
-		header = ['time', 'I', 'W', 'V', 'T']
-	else:
+	if 'device_id' not in parsed_url.keys():
 		raise Exception("Relieved malformed URL data")
+
+	header = ['time', 'I', 'W', 'V', 'T']
+	start_time = end_time = data_type = None
+	if 'type' in parsed_url.keys():
+		data_type = parsed_url['type']
+		header = ['time', parsed_url['type']]
+	if 'start_time' in parsed_url.keys():
+		start_time = parsed_url['start_time']
+	if 'end_time' in parsed_url.keys():
+		end_time = parsed_url['end_time']
+
+	if start_time or end_time or data_type:
+		results = retrieve_within_filters(
+			parsed_url['device_id'],
+			start_time,
+			end_time,
+			data_type,
+		)
+	else:
+		results = retrieve_historical(parsed_url['device_id'])
 
 	return format_data(header, results)
 
 
-def retrieve_by_type(device_id, start_time, end_time, data_type):
+def retrieve_within_filters(device_id, start_time, end_time, data_type):
 	"""
-	Return sensor data of a specific type for a device within a specified timeframe
+	Return sensor data for a device within a specified timeframe
 
 	:param device_id: The serial number of the device in question
 	:param start_time: The start of the time range for which to query for data
@@ -48,24 +50,29 @@ def retrieve_by_type(device_id, start_time, end_time, data_type):
 	:param data_type: The type of data to query for
 	:return: Generator of database row tuples
 	"""
-	query = "SELECT time, data FROM data_raw WHERE serial = %s AND time BETWEEN to_timestamp(%s) AND to_timestamp(%s) AND type = %s;"
-	params = (device_id, start_time, end_time, data_type)
-	rows = perform_query(query, params)
-	return rows
+	params = [device_id]
+	where = None
+	if start_time and end_time:
+		where = "WHERE serial = %s AND time BETWEEN to_timestamp(%s) AND to_timestamp(%s)"
+		params.append(start_time)
+		params.append(end_time)
+	elif start_time:
+		where = "WHERE serial = %s AND time >= to_timestamp(%s)"
+		params.append(start_time)
+	elif end_time:
+		where = "WHERE serial = %s AND time <= to_timestamp(%s)"
+		params.append(end_time)
+	if data_type:
+		if where:
+			where += " AND type = %s"
+		else:
+			where = "WHERE serial = %s AND type = %s"
+		params.append(data_type)
+		query = "SELECT time, data FROM data_raw " + where
+	else:
+		query = write_crosstab(where)
 
-
-def retrieve_within_timeframe(device_id, start_time, end_time):
-	"""
-	Return sensor data for a device within a specified timeframe
-
-	:param device_id: The serial number of the device in question
-	:param start_time: The start of the time range for which to query for data
-	:param end_time: The end of the time range for which to query for data
-	:return: Generator of database row tuples
-	"""
-	query = write_crosstab("WHERE serial = %s AND time BETWEEN to_timestamp(%s) AND to_timestamp(%s)")
-	params = (device_id, start_time, end_time)
-	rows = perform_query(query, params)
+	rows = perform_query(query, tuple(params))
 	return rows
 
 
