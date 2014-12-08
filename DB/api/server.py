4b1c11fa2e0523426cac6ef89fe6c95b9abbe709
Raymond Colebaugh <raymondcolebaugh@gmail.com>
2014-12-07 21:34:46 -0800

    Updated use of TCPServer in API to handle requests in new threads

--- DB/api/server.py
+++ DB/api/server.py
@@ -2,14 +2,18 @@
 
 import http.server
 import socketserver
+import threading
 
 import handler
 
 PORT = 8080
 
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    ''' Handle requests in a separate thread '''
+
 Handler = handler.ApiHandler
 
-httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd = ThreadedTCPServer(("", PORT), Handler)
 
 print("serving at port", PORT)
 httpd.serve_forever()
