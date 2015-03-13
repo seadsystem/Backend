557a334d4c04ce30ccb8caf32adf00ece8318dc8
Ian <git@loosescre.ws>
2015-03-13 15:47:07 -0700

    Added full request dumping.

--- DB/landingzone/eGaugeHandlers/eGaugeHandlers.go
+++ DB/landingzone/eGaugeHandlers/eGaugeHandlers.go
@@ -6,6 +6,7 @@ package eGaugeHandlers
 import (
 	"log"
 	"net/http"
+	"net/http/httputil"
 
 	"github.com/seadsystem/Backend/DB/landingzone/database"
 )
@@ -14,6 +15,12 @@ func HandleRequest(res http.ResponseWriter, req *http.Request, db database.DB) {
 	log.Println("Got request from:", req.RemoteAddr)
 	if req.Method == "POST" {
 		log.Printf("Request from (%s) is of type POST\n", req.RemoteAddr)
-		log.Printf("%+v\n", req.PostForm)
+	}
+
+	data, err := httputil.DumpRequestOut(req, true)
+	if err != nil {
+		log.Println("Error dumping request:", err)
+	} else {
+		log.Println(data)
 	}
 }
