02b9535c9b6ec344565d93355f0986e00f677223
Ian <git@loosescre.ws>
2015-03-28 15:09:40 -0700

    Modifed eGaugeHandler to just dump body.

--- DB/landingzone/eGaugeHandlers/eGaugeHandlers.go
+++ DB/landingzone/eGaugeHandlers/eGaugeHandlers.go
@@ -6,7 +6,8 @@ package eGaugeHandlers
 import (
 	"log"
 	"net/http"
-	"net/http/httputil"
+	//"net/http/httputil"
+	"bytes"
 
 	"github.com/seadsystem/Backend/DB/landingzone/database"
 )
@@ -17,11 +18,17 @@ func HandleRequest(res http.ResponseWriter, req *http.Request, db database.DB) {
 		log.Printf("Request from (%s) is of type POST\n", req.RemoteAddr)
 	}
 
-	data, err := httputil.DumpRequest(req, true)
+	/*data, err := httputil.DumpRequest(req, true)
 	if err != nil {
 		log.Println("Error dumping request:", err)
 	} else {
 		log.Println(string(data))
 		log.Println("b2a:", req.PostFormValue("b2a"))
+	}
+	*/
+	if req.Body != nil {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(req.Body)
+		log.Println(string(buf.Bytes()))
 	}
 }
