0b5d7820f19764cf11e31bebe9712f5e240db137
Ian Gudger <git@loosescre.ws>
2015-09-27 13:33:31 -0700

    Try test-coverage script.

---
+++ DB/landingzone/test-coverage.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Script from https://github.com/gopns/gopns
+
+echo "mode: set" > acc.out
+for Dir in $(find ./* -maxdepth 10 -type d ); 
+do
+	if ls $Dir/*.go &> /dev/null;
+	then
+		returnval=`go test -coverprofile=profile.out $Dir`
+		echo ${returnval}
+		if [[ ${returnval} != *FAIL* ]]
+		then
+    		if [ -f profile.out ]
+    		then
+        		cat profile.out | grep -v "mode: set" >> acc.out 
+    		fi
+    	else
+    		exit 1
+    	fi	
+    fi
+done
+
+$GOPATH/bin/goveralls -coverprofile=acc.out -service=travis-ci
+
+rm -rf ./profile.out
+rm -rf ./acc.out
