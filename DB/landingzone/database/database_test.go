6651476dca5c7416d4f0eee21540c3dabe94277f
Ian <git@loosescre.ws>
2015-09-28 21:22:24 -0700

    Added TestSetMaxOpenConns.

--- DB/landingzone/database/database_test.go
+++ DB/landingzone/database/database_test.go
@@ -1,12 +1,30 @@
 package database
 
-import "testing"
+import (
+	"testing"
+
+	sqlmock "github.com/DATA-DOG/go-sqlmock"
+)
 
 func TestNewMock(t *testing.T) {
 	db, err := NewMock()
 	if err != nil {
 		t.Fatalf("got NewMock() = _, %v, want = _, nil", err)
 	}
+	if err := db.Close(); err != nil {
+		t.Fatalf("got db.Close() = %v, want = nil", err)
+	}
+}
+
+func TestSetMaxOpenConns(t *testing.T) {
+	conn, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("got sqlmock.New() = _, _, %v, want = _, _, nil", err)
+	}
+	mock.ExpectClose()
+	db := DB{conn}
+
+	db.SetMaxOpenConns(5)
 	if err := db.Close(); err != nil {
 		t.Fatalf("got db.Close() = %v, want = nil", err)
 	}
