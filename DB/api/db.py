5d95f46ceb181960ca0481b7495449144f7bd17c
Ian Lofgren <ianlofs@users.noreply.github.com>
2016-01-17 20:51:21 -0800

    update query performer to reflect new postgres install

--- DB/api/db.py
+++ DB/api/db.py
@@ -242,8 +242,7 @@ def perform_query(query, params):
 	"""
 	con = None
 	try:
-		con = psycopg2.connect("dbname='" + DATABASE +
-				"' user='" + USER + "'")
+		con = psycopg2.connect(database = DATABASE, user = USER, host = '/tmp/')
 		cursor = con.cursor()
 		print("Query:", query)
 		print("Parameters:", params)
