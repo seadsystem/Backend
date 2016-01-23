984c9bd4f234d3e1957fc46ae35d111f715bfae6
Ian Lofgren <i.lof33@gmail.com>
2016-01-22 21:14:19 -0800

    hdf5 conversion working

--- DB/classification/dataset_converter.py
+++ DB/classification/dataset_converter.py
@@ -1,52 +1,84 @@
-import pandas
-import numpy as np
+from pandas.io.pytables import HDFStore
+from pandas import DataFrame
 import psycopg2
 
 DATABASE = "seads"
 USER = "ianlofgren"
 TABLE = "data_raw"
 
-store = pandas.io.pytables.HDFStore('store.h5')
+store = HDFStore('store.h5')
 con = psycopg2.connect(database = DATABASE, user = USER)
 cursor = con.cursor()
-cursor.execute("select * from data_raw where serial = 466419816 limit 50;")
+cursor.execute(" select serial, type, data - lag(data, 14) over \
+                 (order by time), time, device from data_raw where \
+                 serial = 466419818 and device != 'PowerS' and device \
+                 != 'PowerG' limit 56 ;")
 records = cursor.fetchall()
 
-#print(str(records[0]))
+for row in records:
+    print(str(row) + '\n')
+
 
 #collect all entries with matching deviceID and timestamp together
-result = []
-for record in records:
+results = []
+for record in records[14:]:
     to_insert = []
-    for row in result:
+    for row in results:
         if row[0] == record[3] and row[1] == record[0]:
             to_insert = row
     if to_insert == []:
         to_insert.append(record[3])
         to_insert.append(record[0])
         to_insert.append([record[1], record[2]])
-        result.append(to_insert)
+        results.append(to_insert)
     else:
         to_insert.append([record[1], record[2]])
 
-#add together all entries with matching power types
-print(str(result) + "\n\n")
+#add together all entries with matching types
+indexes = []
+values = {}
+for row in results:
+    indexes.append(row[0])
+    del row[:2]
+    temp = {}
+    for data in row:
+        if data[0] in temp:
+            temp[data[0]] += data[1]
+        else:
+            temp[data[0]] = data[1]
+    for key,value in temp.items():
+        if key in values:
+            values[key].append(value)
+        else:
+            values[key] = [value]
 
+dfs = DataFrame(values, index=indexes)
+store['dfs'] = dfs
+print(str(store['dfs']))
+
+"""
 hdf5 = []
 for row in result:
-    row.remove(row[1])
-    temp = [row[0]]
+    temp = {}
     for i, element in enumerate(row[1:], start=1):
-        signal = True
-        for to_find in temp[1:]:
-            #print(str(to_find) + str(element) + "\n")
-            if to_find[0] == element[0]:#fix
-                signal = False
-                to_find[1] += element[1]
-                #print(str(element) + "  " + str(thing) + "\n")
-                #row.remove(thing)
-        if signal:
-            temp.append(element)
+        if element[0] in temp:
+            temp[element[0]] += element[1]
+        else:
+            temp[element[0]] = element[1]
     hdf5.append(temp)
+
+values = []
+index = []
+columns = []
 for row in hdf5:
-    print(str(row) + "\n")
+    values.append([value for key, value in row.items() if key != 'time'])
+    index.append([value for key, value in row.items() if key == 'time'])
+    columns += [key for key, value in row.items() if key not in columns]
+    #print(str(row) + "\n")
+columns.remove('time');
+#print(str(values))
+#print(str(index))
+#print(str(columns))
+#dfs =
+#print(str(dfs))
+"""
