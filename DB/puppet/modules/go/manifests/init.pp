fdbf81eceaccbd2d50a3c2f257095dd322638c80
Raymond Colebaugh <raymondcolebaugh@gmail.com>
2016-02-07 14:53:01 -0800

    Updated Go Puppet module to use godeb to install latest Go version

--- DB/puppet/modules/go/manifests/init.pp
+++ DB/puppet/modules/go/manifests/init.pp
@@ -1,9 +1,18 @@
 # Install golang and libpq for the landingzone user
 class go {
-  # Install go package
-  package {'golang':
-    ensure  => installed,
-    require => User['landingzone'],
+  # Install godeb and go
+  $bin_url = 'https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz'
+  exec {'install-godeb':
+    command => "curl ${bin_url} | tar zxf - > godeb",
+    cwd     => '/usr/local/sbin',
+    creates => '/usr/local/sbin/godeb',
+  }
+
+  exec {'godeb install':
+    require => Exec['install-godeb'],
+    cwd     => '/tmp',
+    timeout => 600,
+    unless  => 'which go',
   }
 
   # Ensure go directory exists for landingzone user
@@ -16,12 +25,12 @@ class go {
   # Install go pq library
   exec {'go-pq':
     command => 'env GOPATH=/home/landingzone/go go get -u github.com/lib/pq',
-    require => Package['golang'],
+    require => Exec['godeb install'],
   }
 
   # Install go sqlmock library
   exec {'go-sqlmock':
     command => 'env GOPATH=/home/landingzone/go go get -u github.com/DATA-DOG/go-sqlmock',
-    require => Package['golang'],
+    require => Exec['godeb install'],
   }
 }
