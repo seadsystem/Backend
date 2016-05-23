5b38807616d16fc01dbd54bf672d2de59dd2b9a0
Nikita Sokolnikov <nsokolnikov1995@gmail.com>
2016-05-22 20:19:43 -0700

    integrating into the baseclassifier

--- DB/classification/models.py
+++ DB/classification/models.py
@@ -109,7 +109,7 @@ class BaseClassifier(object):
 
     @classmethod
     @Memoized
-    def get_model(cls):
+    def get_model(cls, model_type="RandomForestClassifier"):
         try:
             con = psycopg2.connect(database=cls.DATABASE, user=cls.USER)
         except psycopg2.Error as e:
@@ -117,8 +117,9 @@ class BaseClassifier(object):
 
         try:
             cursor = con.cursor()
-            query = "SELECT * FROM classifier_model ORDER BY created_at DESC LIMIT 1;"
-            cursor.execute(query)
+            params = {'model_type': model_type}
+            query = "SELECT * FROM classifier_model WHERE type=%(model_type)s ORDER BY created_at DESC LIMIT 1;"
+            cursor.execute(query, params)
             model_row = cursor.fetchall()
         except psycopg2.Error as e:
             raise IOError("Model lookup failed", e)
@@ -176,6 +177,37 @@ class BaseClassifier(object):
                 con.close()
 
     @classmethod
+    def aggregate_data(cls):
+        con = psycopg2.connect(database=cls.DATABASE, user=cls.USER)
+        cursor = con.cursor()
+        params = {'panel': 'Panel1',
+                  'id': None}
+        query = "select * from data_label"
+        cursor.execute(query, params)
+        labels = cursor.fetchall()
+
+        query = "select * from data_raw order by time desc limit 100;"
+        cursor.execute(query)
+        dataraw = cursor.fetchall()
+
+        con.close()
+
+        for i in range(len(dataraw)):
+                dataraw[i] = list(dataraw[i])
+        result = []
+        for datum in dataraw:
+                for label in labels:
+                        #datum[3] is the timestamp of the datum
+                        #label 1, 2 are the limits of the label
+                        #label 3 is the label
+                        if(datum[3] < label[2] and datum[3] > label[1]):
+                                datum.append(label[3])
+        for datum in dataraw:
+                if(len(datum) > 5):
+                        result.append(datum)
+        return result
+
+    @classmethod
     def training_data(cls, panel="Panel1"):
         """
         :summary: grabs power data in data_raw that corresponds to the labels in data_label
@@ -194,7 +226,6 @@ class BaseClassifier(object):
                     WHERE data_raw.serial=data_label.serial  \
                     AND data_raw.type='P'  \
                     AND data_raw.device=%(device)s \
-                    AND time BETWEEN data_label.start_time AND data_label.end_time;"
             cursor.execute(query, {'device': panel})
             return cursor.fetchall()
         except psycopg2.Error as e:
