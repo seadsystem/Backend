acd65954eac3066b27eec2b34f355089daeef29b
Nikita Sokolnikov <nsokolnikov1995@gmail.com>
2016-05-22 18:39:21 -0700

    some phython

---
+++ DB/classification/aggregator_test.py
@@ -0,0 +1,22 @@
+import datetime
+import pickle
+import psycopg2
+import uuid
+import functools
+
+
+
+database = 'seads'
+user = 'seadapi'
+def aggregate_data():
+	con = psycopg2.connect(database=database, user=user)
+	cursor = con.cursor()
+	params = {'panel': 'Panel1',
+                      'id': None}
+    query = "select * from data_label"
+    cursor.execute(query, params)
+    results = cursor.fetchall()
+    print(results)
+
+
+aggregate_data()
