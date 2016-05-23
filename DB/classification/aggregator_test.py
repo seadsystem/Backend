f4da109e539538402dd8f0179b961512cbb48302
cpeter256 <cpeter256@gmail.com>
2016-05-22 19:59:33 -0700

    worky

--- DB/classification/aggregator_test.py
+++ DB/classification/aggregator_test.py
@@ -17,19 +17,21 @@ def aggregate_data():
         cursor.execute(query, params)
         labels = cursor.fetchall()
 
-        query = "select * from data_raw order by time asc limit 50;"
+        query = "select * from data_raw order by time desc limit 100;"
         cursor.execute(query)
         dataraw = cursor.fetchall()
         
-
+        result = []
         for datum in dataraw:
-        	for label in labels:
-        		#datum[3] is the timestamp of the datum
-        		#label 1, 2 are the limits of the label
-        		#label 3 is the label
-        		if(datum[3] < label[2] and datum[3] > label[1]):
-        			datum.append(label[3])
-        result = [elem for elem in dataraw if len(elem) > 5]
+                for label in labels:
+                        #datum[3] is the timestamp of the datum
+                        #label 1, 2 are the limits of the label
+                        #label 3 is the label
+                        print("" + str(datum[3]) + " < " + str(label[2]) + " : " + str(datum[3]<label[2]) + " | " + str(datum[3]) + " > " + str(label[1]) + " : " + str(datum[3]>label[1]))
+                        if(datum[3] < label[2] and datum[3] > label[1]):
+                                result.append(datum + (label[3],))
+                                print(datum)
+#        result = [elem for elem in dataraw if len(elem) > 5]
         print(result)
 
 
